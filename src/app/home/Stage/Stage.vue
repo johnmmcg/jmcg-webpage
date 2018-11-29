4e53b63c009b34d1fb843026ce04d253d7e9270c
John <john.maurer.mcguinness@gmail.com>
2018-11-29 11:47:02 -0500

    updating mainTitle

--- src/app/home/Stage/Stage.vue
+++ src/app/home/Stage/Stage.vue
@@ -11,10 +11,9 @@
         <vue-grid-row>
           <vue-grid-item class="vueGridItem">
             <div :class="$style.textBlock">
-              <div :class="$style.topLeftBorder"></div>
-              <div :class="$style.BottomRightBorder"></div>
-
-              <h1 :class="mainTitle" class="animated fade">John McGuinness</h1>
+              <div :class="mainTitle">
+                <h1>John McGuinness</h1>
+              </div>
               <h2 :class="$style.title">Front End Web Developer</h2>
               <div :class="$style.locationContainer">
                 <h3 :class="$style.subTitle">Philadelphia    Pennsylvania</h3>
@@ -287,39 +286,56 @@
   }
 
   .mainTitle {
-    font-family: $font-family-headings;
-    font-weight: 700;
-    font-size: 8rem;
-    letter-spacing: .5rem;
-    padding: .5rem 1rem;
-    margin-top: .25rem;
+    padding: 3rem;
     width: auto;
     display: inline-block;
-    color: $text-color-inverse;
     background-image: linear-gradient(120deg, $brand-primary 0%, $brand-accent 70%);
     background-repeat: no-repeat;
     background-size: 100% 0.1em;
     background-position: 50% 88%;
-    animation: mainTitleEntry 10s ease-in-out;
+    animation: mainTitleEntry 5s ease-in-out;
     transition: all 0.25s ease-in;
 
-    @include media(tabletPortrait, max) {
-      font-size: 4rem;
+    h1 {
+      font-family: $font-family-headings;
+      font-weight: 700;
+      font-size: 8rem;
+      letter-spacing: .5rem;
+      margin: .25rem;
+      padding: 1rem 3rem;
+      padding-top: 0.25rem;
+      border: 2px solid $bg-color;
+      color: $text-color-inverse;
+
+      @include media(tabletPortrait, max) {
+        font-size: 4rem;
+      }
     }
   }
 
   .mainTitleLoaded {
-    font-size: 8rem;
-    letter-spacing: .5rem;
-    margin-top: .25rem;
-    padding: .5rem 1rem;
+    padding: 3rem;
     background-image: linear-gradient(120deg, $brand-primary 0%, $brand-accent 70%);
     background-position: 100% 88%;
-    background-size: 100% 95%;
+    background-size: 100%;
     transition: all 0.25s ease-in;
 
-    @include media(tabletPortrait, max) {
-      font-size: 4rem;
+    h1 {
+      font-family: $font-family-headings;
+      font-weight: 700;
+      font-size: 8rem;
+      letter-spacing: .5rem;
+      margin: .25rem;
+      padding: 1rem 3rem;
+      padding-top: 0.25rem;
+      color: $text-color-inverse;
+      border: 2px solid $bg-color;
+      position: relative;
+      transition: .25s linear;
+
+      @include media(tabletPortrait, max) {
+        font-size: 4rem;
+      }
     }
   }
 
@@ -369,8 +385,11 @@
   }
 
   .linksContainer {
+    padding: 1rem 2rem;
     padding-bottom: 6rem;
     height: auto;
+    max-width: 1000px;
+    margin: auto;
   }
 
   .linkContainer {
@@ -563,30 +582,24 @@
 
   @keyframes mainTitleEntry {
     0% {
-      font-family: $font-family-headings;
-      font-weight: 700;
-      padding: .5rem 1rem;
-      width: auto;
-      display: inline-block;
-      background-repeat: no-repeat;
-      background-size: 0% 0.1em;
+      background-size: 0% 0%;
       background-position: 50% 88%;
-      transition: all 0.25s ease-out;
     }
 
-    5% {
+    15% {
       background-size: 100% 0.1em;
-      background-position: 0% 88%;
+      background-position: 50% 88%;
+      transition: all .25s ease-in-out;
     }
 
-    8% {
+    30% {
       background-position: 100% 88%;
-      background-size: 100% 95%;
+      background-size: 100% 100%;
     }
 
     100% {
       background-position: 100% 88%;
-      background-size: 100% 95%;
+      background-size: 100% 100%;
     }
   }
 
@@ -611,5 +624,38 @@
     100% {
       opacity: 1;
     }
+  }
+
+  .mainTitleLoaded::after {
+  	content:'';
+    top:0;
+  	transform:translateX(100%);
+  	width: 100%;
+  	height: 80%;
+  	position: absolute;
+  	z-index:1;
+  	animation-name: slide;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+    animation-delay: .5s;
+
+    /*
+    CSS Gradient - complete browser support from http://www.colorzilla.com/gradient-editor/
+    credit to codepen by Kieran Hunter
+    */
+    background: -moz-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(128,186,232,0) 99%, rgba(125,185,232,0) 100%); /* FF3.6+ */
+  	background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(255,255,255,0)), color-stop(50%,rgba(255,255,255,0.8)), color-stop(99%,rgba(128,186,232,0)), color-stop(100%,rgba(125,185,232,0))); /* Chrome,Safari4+ */
+  	background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Chrome10+,Safari5.1+ */
+  	background: -o-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Opera 11.10+ */
+  	background: -ms-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* IE10+ */
+  	background: linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* W3C */
+  	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#007db9e8',GradientType=1 ); /* IE6-9 */
+  }
+
+  /* animation */
+
+  @keyframes slide {
+  	0% {transform:translateX(-150%);}
+  	100% {transform:translateX(150%);}
   }
 </style>
